a2729104bfd40ca1a9de2c1d2b73cc1c046d7fdb
fauzan <fauzan@dawrat.com>
2023-08-16 13:44:04 +0530

    Final commit

--- movierater/api/urls.py
+++ movierater/api/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include
 from django.urls import path
 from rest_framework import routers
+from .views import MovieViewSet, RatingViewSet, UserViewSet
 
 router = routers.DefaultRouter()
+router.register('movies', MovieViewSet)
+router.register('ratings', RatingViewSet)
+router.register('users', UserViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
